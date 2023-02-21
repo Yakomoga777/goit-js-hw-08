84c0ecabb83130c3f1f6c94c8f5d15786b681c7a
Roman Petrenko <rpetrenko0105@gmail.com>
2023-02-21 18:27:45 +0200

    add setcurrentTime

--- src/js/002-video.js
+++ src/js/002-video.js
@@ -38,3 +38,21 @@ const onPlaySetCrntTime = function (data) {
 
 player.on('timeupdate', onPlaySetCrntTime);
 // console.log(onPlaySetCrntTime());
+
+//* Метод для встановелення часу для початку відтворювання
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
